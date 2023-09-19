ccc328a2d314539bb1407d7286ffee93eaecc804
Igor <117585705+igorjosecm@users.noreply.github.com>
2023-09-19 16:13:45 -0300

    desafio feito

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,1 +1,20 @@
-alert("Olá")
+function carregar(){
+
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        img.src = 'imagens/foto-manha.png'
+        document.body.style.backgroundColor = '#fde1b4'
+    }else if(hora >= 12 && hora <= 18){
+        img.src = 'imagens/foto-tarde.png'
+        document.body.style.backgroundColor = '#e78b6d'
+    }else{
+        img.src = 'imagens/foto-noite.png'
+        document.body.style.backgroundColor = '#343549'
+    }
+}
