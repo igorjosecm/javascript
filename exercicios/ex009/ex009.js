27795112b2278a3435609b01c457558168450baf
Igor <117585705+igorjosecm@users.noreply.github.com>
2023-09-19 14:31:13 -0300

    mais avancos

--- exercicios/ex009/ex009.js
+++ exercicios/ex009/ex009.js
@@ -1,7 +1,7 @@
-var país = 'Brasil'
+/* var país = 'Brasil'
 console.log(`Vivendo em ${país}`)
 if (país == 'Brasil'){
     console.log("Brasileiro!")
 }else{
     console.log("Estrangeiro!")
-}
+} */
