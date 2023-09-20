4c7a712680cb6c58a5205febda6aefaa2795e1ee
Igor <117585705+igorjosecm@users.noreply.github.com>
2023-09-20 11:32:03 -0300

    Update script.js

--- exercicios/ex018/script.js
+++ exercicios/ex018/script.js
@@ -5,10 +5,26 @@ function contar(){
     let res = document.getElementById('res')
 
     if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        alert('[ERRO] Faltam dados!')
+        //alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     } else{
-        res.innerHTML = `Contando: `
+        res.innerHTML = `Contando: <br>`
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0) {
+            alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
+        if(i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML +=`\u{1F3C1}`
     }
-
-
 }
