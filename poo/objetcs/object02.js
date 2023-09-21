fed057d07c7d5c8697baf5c89f1bf14bcd78802f
Igor <117585705+igorjosecm@users.noreply.github.com>
2023-09-21 17:43:29 -0300

    Update object02.js

--- poo/objetcs/object02.js
+++ poo/objetcs/object02.js
@@ -2,20 +2,29 @@ class User{
     constructor(email, name){
         this.email = email;
         this.name = name;
+        this.score = 0;
     }
     login(){
         console.log(this.email, 'just logged in');
+        return this;
     }
     logout(){
         console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score)
+        return this;
     }
 }
 
 let userOne = new User('ryu@ninjas.com', 'Ryu');
 let userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
 
-console.log(userOne.login());
-console.log(userTwo.logout());
+userOne.login().updateScore().updateScore().logout();
+
+
 
 // the 'new' keyword
 // - creates a new empty object {}
