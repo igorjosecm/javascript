a78eda40454322e366a36cfbd5d82c44530c5f73
Igor <117585705+igorjosecm@users.noreply.github.com>
2023-10-04 11:47:19 -0300

    mais js

---
+++ b7web/aulas/aulas 21 - 40/assets034/script.js
@@ -0,0 +1,8 @@
+let cartao = "1234123412341234";
+
+let lastDigits = cartao.slice(-4);
+
+let cartaoMascarado = lastDigits.padStart(16, "*");
+
+
+console.log(`Este é o seu cartão? ${cartaoMascarado}`)
